7166f88a95c7aad49f9135f9f977fc6b6ba0d1a6
小聪 <95623656+suxiaocong2020@users.noreply.github.com>
2022-06-24 20:55:58 +0800

    Add files via upload

---
+++ test02/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace RectangleApplication
+{
+    class Rectangle
+    {
+        //成员变量
+        private double radius;
+
+        public void Acceptdetails()
+        {
+            Console.WriteLine("请输入半径：");
+            radius = Convert.ToDouble(Console.ReadLine());
+        }
+
+        public double GetArea()
+        {
+            return radius * radius  * 3.1415;
+        }
+
+        public double GetPerimeter()
+        {
+            return radius * 2 * 3.1415;
+        }
+
+        public void Display()
+        {
+            Console.WriteLine("面积： {0}", GetArea() );
+            Console.WriteLine("周长： {0}", GetPerimeter());
+        }
+
+    }//end class Rectangle    
+    class ExecuteRectangle
+    {
+        static void Main(string[] args)
+        {
+            Rectangle r = new Rectangle();
+            r.Acceptdetails();
+            r.Display();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ test_01/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace RectangleApplication
+{
+    class Rectangle
+    {
+        //成员变量
+        public double length;
+        public double width;
+
+        public double GetArea()
+        {
+            return length * width;
+        }
+        public void Display()
+        {
+            Console.WriteLine("长度： {0}", length);
+            Console.WriteLine("宽度： {0}", width);
+            Console.WriteLine("面积： {0}", GetArea());
+        }
+    }// Rectangle 结束
+
+    class ExecuteRectangle
+    {
+        static void Main(string[] args)
+        {
+            Rectangle r = new Rectangle();
+            r.length = 4.5;
+            r.width = 3.5;
+            r.Display();
+            Console.ReadLine();
+        }
+    }
+}
