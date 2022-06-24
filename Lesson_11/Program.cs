aaf534a26e25fa14a9c5beebad03f454af7a7d85
小聪 <95623656+suxiaocong2020@users.noreply.github.com>
2022-06-24 20:54:45 +0800

    Add files via upload

---
+++ Lesson_11/Program.cs
@@ -0,0 +1,146 @@
+﻿using System;
+
+namespace Lesson_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //for (int i = 1; i <= 100; i++)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            #region 1~100所有偶数的和
+            //int add = 0;
+            //for (int i = 1; i <= 100; i++)
+            //{
+            //    if (i % 2 == 0)
+            //    {
+            //        add += i;
+            //    }
+            //}
+            //Console.WriteLine(add);
+            #endregion
+
+            #region 找出100~999之间的水仙花数、
+            ////例：153 = 1x1x1 + 5x5x5 + 3x3x3
+            //int A;
+            //int B;
+            //int C;
+            //for (int i = 100; i < 999; i++)
+            //{
+            //    string ABC =  i.ToString();
+            //    A = int.Parse( ABC.Substring(0, 1));
+            //    B = int.Parse(ABC.Substring(1, 1));
+            //    C = int.Parse(ABC.Substring(2));
+            //    if ( i == A*A*A+B*B*B+C*C*C)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //}
+
+            ////-------------------------------------------
+
+            //for (int i = 100; i < 999; i++)
+            //{
+            //    //用余数来取个十百位对应的数
+            //    int A = i / 100;    //百位数
+            //    int B = i % 100/10; //十位数
+            //    int C = i % 10;     //个位数
+            //    if (i == A * A * A + B * B * B + C * C * C)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+
+            //}
+
+            #endregion
+
+            #region 打印九九乘法表
+            //for (int x = 1; x <= 9; x++)
+            //{
+            //    for (int y = 1; y <= 9; y++)
+            //    {
+            //        Console.Write("{0}x{1}={2}\t",y,x,x*y);
+            //        if (y>=x)
+            //        {
+            //            break;
+            //        }
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            #endregion
+
+            #region 10*10空心方阵
+            //for (int i = 1; i <= 10; i++)
+            //{
+            //    if (i == 1)
+            //    {
+            //        Console.WriteLine("**********");
+            //    }
+            //    else if (i == 10)
+            //    {
+            //        Console.WriteLine("**********");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("*        *");
+            //    }
+            //}
+            #endregion
+
+            #region 输出10*10三角形方阵
+            //string add = "";
+            //string sanjiao = "*";
+            //for (int i = 1; i <=10; i++)
+            //{
+            //    add += sanjiao;
+            //    Console.WriteLine(add);
+
+            //}
+
+            ////----------------------------------
+            ///
+            //for (int i = 1; i <=10; i++) 
+            //{
+            //    for (int j = 1; j <= i; j++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+            #endregion
+
+            #region 输出10*10空心方阵
+
+            ////           *
+            ////          ***
+            ////         *****
+            ////        *******
+            ////       *********
+            ////      ***********
+            ////     *************
+            ////    ***************
+            ////   *****************
+            ////  *******************
+            //for (int i = 1; i <=10; i++)  //行
+            //{
+            //    //列
+            //    for (int n = 1; n <= 10-i; n++) //打印空格使得三角形对齐
+            //    {
+            //        Console.Write(" ");
+            //    }
+            //    for (int j = 1; j <= 2*i-1 ; j++)  //高中学的等差数列 2n-1 [1,3,5,7,9,11....]
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+            //二级你噢俄海军哦i额黑iohj21
+            #endregion
+
+        }
+    }
+}
---
+++ Lesson_9/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+
+namespace Lesson_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region 异常捕捉
+            //异常捕捉
+            //try
+            //{
+            //想要捕捉异常的代码
+            //    string str = console.readline();
+            //    int.parse(str);
+
+            //}
+            //catch
+            //{
+            //    console.writeline("请输入合法数字");
+            //}
+            #endregion
+
+            #region 时间转换 练习
+            // 987652 转为天时分秒
+            int s = 987652 % 60;
+            int m = (987652 / 60) % 60;
+            int t = (987652 / 3600) % 24;
+            int d = 987652 / (3600 * 24);
+            //Console.WriteLine(d + "天\t" + t + "时\t" + m + "分\t" + s + "秒\t");
+
+            //字符串拼接
+            //string sba = string.Format("{0}天\t{1}时\t{2}分\t{3}秒\t", d, t, m, s);
+            //Console.WriteLine(sba);
+            
+            //控制台默认调用
+            Console.WriteLine("{0}天\t{1}时\t{2}分\t{3}秒\t",d,t,m,s);
+            #endregion
+
+            #region 三目运算符
+            int a = 10;
+            bool b = true;
+            a=b ? 20 : 30;
+            #endregion
+
+            #region 绩效 练习
+            int tang = 4000;
+            Console.WriteLine("请输入唐老师的课程评级(A,B,C,D,E)");
+            string lever = Console.ReadLine();
+            switch (lever)
+            {
+                case "A":
+                    tang += 500;
+                    break;
+                case "B":
+
+                    break;
+                case "C":
+                    tang -= 300;
+                    break;
+                case "D":
+                    tang -= 500;
+                    break;
+                case "E":
+                    tang -= 800;
+                    break;
+                default:
+                    Console.WriteLine("请输入有效评级(A,B,C,D,E)");
+                    break;
+                
+            }
+            Console.WriteLine("唐老师工资为:" + tang);
+            #endregion
+
+            #region 登录 练习
+            string sxc = "suxiaocong";
+            string poss = "12352";
+
+            string inpna = "2";
+            string inpps = "1";
+
+            while (!(inpna==sxc&& inpps==poss))
+            {
+                Console.WriteLine("请玩家输入用户名");
+                inpna = Console.ReadLine();
+                Console.WriteLine("请玩家输入密码");
+                inpps = Console.ReadLine();
+                if (inpna != sxc || inpps != poss)
+                {
+                    Console.WriteLine("账户或密码输入错误 请重新输入");
+                }
+
+            }
+            Console.WriteLine("suxiaocong登录成功");
+            #endregion
+        }
+    }
+}
